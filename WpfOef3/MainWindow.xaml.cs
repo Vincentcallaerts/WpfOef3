f156cb35a23b46548531544e09d5305bebf2a821
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-05-06 16:01:01 +0200

    Final push of the day

--- WpfOef3/MainWindow.xaml.cs
+++ WpfOef3/MainWindow.xaml.cs
@@ -22,10 +22,27 @@ namespace WpfOef3
     {
         public List<string> Geslacht { get; set; }
         public List<string> LeeftijdsGroepen { get; set; }
+        private List<Persoon> Persoons { get; set; }
+        private List<CheckBox> CheckBoxes { get; set; }
+
+        public string TextBoxTextCopied { get; set; }
+        public string TextBoxTextCurrentValue { get; set; }
+        public string TextBoxTextOldValue { get; set; }
+
+
+
 
         public MainWindow()
         {
             InitializeComponent();
+
+            CheckBoxes = new List<CheckBox>();
+
+            CheckBoxes.Add(cbRood);
+            CheckBoxes.Add(cbGroen);
+            CheckBoxes.Add(cbGeel);
+            CheckBoxes.Add(cbBlauw);
+            CheckBoxes.Add(cbOrange);
 
             Geslacht = new List<string>();
 
@@ -44,6 +61,18 @@ namespace WpfOef3
             cmbGeslacht.SelectedIndex = 0;
             cmbLeeftijd.SelectedIndex = 0;
 
+            Persoons = new List<Persoon>();
+
+            Persoons.Add(new Persoon("Ken", "Field", false));
+            Persoons.Add(new Persoon("Ben", "Field", true));
+            Persoons.Add(new Persoon("Gwen", "Field", false));
+            Persoons.Add(new Persoon("Ben", "Field", true));
+            Persoons.Add(new Persoon("Gwen", "Field", false));
+            Persoons.Add(new Persoon("Ben", "Field", true));
+            Persoons.Add(new Persoon("Gwen", "Field", false));
+
+            lbPerson.ItemsSource = Persoons;
+            
         }
 
         private void Click_Click(object sender, RoutedEventArgs e)
@@ -66,12 +95,85 @@ namespace WpfOef3
 
         private void btnBlue_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(lbGoBlue.Foreground.ToString());
 
             if (lbGoBlue.Foreground.ToString() == "#FF000000")
             {
                 lbGoBlue.Foreground = Brushes.AliceBlue;
+                btnBlue.Content = "Come Back";
             }
+            else
+            {
+                lbGoBlue.Foreground = Brushes.Black;
+                btnBlue.Content = "Be Gone";
+
+            }
+        }
+
+        private void miClear_Click(object sender, RoutedEventArgs e)
+        {
+
+            tbMenuManipulation.Text = String.Empty;
+
+        }
+
+        private void miCopy_ClickCopie(object sender, RoutedEventArgs e)
+        {
+            TextBoxTextCopied = tbMenuManipulation.Text;
+        }
+
+        private void miPaste_ClickPaste(object sender, RoutedEventArgs e)
+        {
+            tbMenuManipulation.Text += TextBoxTextCopied;
+        }
+        private void miPaste_ClickRedo(object sender, RoutedEventArgs e)
+        {
+            tbMenuManipulation.Text = TextBoxTextOldValue;
+
+        }
+        private void miPaste_ClickUndo(object sender, RoutedEventArgs e)
+        {
+            tbMenuManipulation.Text = TextBoxTextOldValue;
+        }
+
+        private void tbMenuManipulation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox text = sender as TextBox;
+
+            TextBoxTextOldValue = TextBoxTextCurrentValue;
+            TextBoxTextCurrentValue = tbMenuManipulation.Text;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+            
+            switch (check.Content.ToString())
+            {
+                case "Rood":
+                    cbRood.Foreground = Brushes.Red;
+                    break;
+                case "Groen":
+                    cbGroen.Foreground = Brushes.Green;
+
+                    break;
+                case "Geel":
+                    cbGeel.Foreground = Brushes.Yellow;
+
+                    break;
+                case "Blauw":
+                    cbBlauw.Foreground = Brushes.Blue;
+
+                    break;
+                case "Orange":
+                    cbOrange.Foreground = Brushes.Orange;
+
+                    break;
+            }
+        }
+
+        private void btnCheckers_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Checkbox Rood : {CheckBoxes[0].IsChecked}\nCheckbox Groen : {CheckBoxes[1].IsChecked}\nCheckbox Geel : {CheckBoxes[2].IsChecked}\nCheckbox Blauw : {CheckBoxes[3].IsChecked}\nCheckbox Orange : {CheckBoxes[4].IsChecked}\n");
         }
     }
 }
---
+++ WpfOef3/Persoon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOef3
+{
+    class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public bool LoggedIn { get; set; }
+        public string FullName { get; set; }
+        public string ImagePath { get; set; }
+
+        public Persoon(string voornaam, string achternaam, bool loggedIn)
+        {
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            LoggedIn = loggedIn;
+
+            FullName = Voornaam + " " + Achternaam;
+
+            if (loggedIn)
+            {
+                ImagePath = "Images/emo-1.png";
+            }
+            else
+            {
+                ImagePath = "Images/emo-3.png";
+
+            }
+        }
+    }
+}
